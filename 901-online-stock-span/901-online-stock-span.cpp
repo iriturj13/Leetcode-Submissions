b2e51ae5dae29a72887c7cbe823ae7475004b68a
Rituraj Anand <79366792+iriturj13@users.noreply.github.com>
2022-06-20 22:10:16 +0530

    Time: 472 ms (7.77%), Space: 84.2 MB (62.57%) - LeetHub

---
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -0,0 +1,26 @@
+class StockSpanner {
+public:
+    StockSpanner() {
+        
+    }
+    
+    
+    stack<pair<int, int>> s; // this will store the NGL element and its index
+    int next(int price) {
+        int res = 1;
+        while (!s.empty() && s.top().first <= price) {
+            res += s.top().second;
+            s.pop();
+        }
+        s.push({price, res});
+        return res;
+    }
+        
+    
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */

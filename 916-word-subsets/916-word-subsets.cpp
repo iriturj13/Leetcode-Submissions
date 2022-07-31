8d56438aff3b05939092aba4b93639417ea74506
Rituraj Anand <79366792+iriturj13@users.noreply.github.com>
2022-07-31 10:43:00 +0530

    Time: 381 ms (37.22%), Space: 128 MB (25.99%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> getFrequencyTable(string &s)
+    vector<int> getFreqTable(string &s)
     {
         vector<int> f(26);
         for(auto c: s)f[c - 'a']++;
@@ -10,28 +10,28 @@ public:
         vector<vector<int>> fre(words2.size(), vector<int> (26));
         for(int i = 0; i < words2.size(); i++)
         {
-            fre[i] = getFrequencyTable(words2[i]);
+            fre[i] = getFreqTable(words2[i]);
         }
         
         //get maximum frequency for every character from 'a' to 'z' present in words2
-        vector<int> maxF(26);
+        vector<int> maxFr(26);
         for(int i = 'a'; i <= 'z' ;i++)
         {
             int mx = 0;
             for(int j = 0; j < words2.size(); j++)mx = max(mx, fre[j][i - 'a']);
-            maxF[i - 'a'] = mx;
+            maxFr[i - 'a'] = mx;
         }
         
         //now we just need to iterate over strings in words1 and find out if that string has frequency array bigger than maxF
         vector<string> ans;
         for(int i = 0 ;i < words1.size(); i++)
         {
-            vector<int> temp = getFrequencyTable(words1[i]);
-            bool isValid = true;
+            vector<int> temp = getFreqTable(words1[i]);
+            bool Valid = true;
             for(int j = 0 ;j < 26;j ++){
-                if(temp[j] < maxF[j])isValid = false;
+                if(temp[j] < maxFr[j])Valid = false;
             }
-            if(isValid)ans.push_back(words1[i]);
+            if(Valid)ans.push_back(words1[i]);
         }
         return ans;
     }

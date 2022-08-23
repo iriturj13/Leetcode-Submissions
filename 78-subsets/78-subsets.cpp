28ee733d49a1236e75b312b88b763493fc47521f
Rituraj Anand <79366792+iriturj13@users.noreply.github.com>
2022-08-23 19:06:38 +0530

    Time: 16 ms (5.45%), Space: 17.2 MB (8.56%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) { 
-       vector<vector<int>>output(1);
-       set<vector<int> >s;
-       int n= nums.size();
-       int size=find(0,nums,n,output,s);
-       sort(output.begin()+1,output.end());
-       return output;   
-   }     
+    
+    vector<vector<int>> ans;
+    void solve(vector<int> ip,vector<int> op)
+    {
+       if(ip.size()==0)
+       {
+          ans.push_back(op); 
+           return;
+       }
+        vector<int> op1=op;
+        vector<int> op2=op;
         
-   int find(int i, vector<int>&nums, int n,vector<vector<int>>&output, set<vector<int> >&s)
-   {
-       if(i==n)// reached at the end of the nums array
-       {
-           return 1;
-       }
-       int small_size=find(i+1,nums,n,output,s);//recursive call
-       
-       for(int j=0;j<small_size;j++)   //small calculation.
-       {
-           vector<int>temp;              
-           temp.push_back(nums[i]);
-           for(int k=0;k<output[j].size();k++)
-           {
-               temp.push_back(output[j][k]);
-           }
-           sort(temp.begin(),temp.end());
-           if(s.find(temp)==s.end())//check whether already
-           {                        //present or not
-               output.push_back(temp); 
-               
-               s.insert(temp);
-           }
-       }
-       
-       return output.size(); // return final size of output nums    
-   }
+        op2.push_back(ip[0]);
+        ip.erase(ip.begin()+0);
+        solve(ip,op1);
+        solve(ip,op2);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+     vector<int> op;
+     solve(nums,op);
+     return ans; 
+    }
 };

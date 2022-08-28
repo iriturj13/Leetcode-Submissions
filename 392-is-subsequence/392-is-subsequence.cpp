00f1aebf45a089a2e74c1bb67a1a480e6cfd19cf
Rituraj Anand <79366792+iriturj13@users.noreply.github.com>
2022-08-28 16:54:32 +0530

    Time: 3 ms (51.41%), Space: 6.4 MB (68.83%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
-    int helper(string x, string y) {
-        int m = x.size();
-        int n = y.size();
-        int dp[m+1][n+1];
-        for(int i = 0; i<=m; i++)
-        {
-            dp[i][0]=0;
-        }
-        for(int i = 0; i<=n; i++)
-        {
-            dp[0][i]=0;
-        }
-        for(int i = 1; i<=m; i++){
-            for(int j = 1; j<=n; j++){
-                if(x[i-1]==y[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        int i = 0, j = 0;
+        while(i < m && j < n) {
+            if(s[i] == t[j]) {
+                i++;
             }
+            j++;
         }
-        return dp[m][n];
+        return i == m ? 1 : 0;
     }
-    bool isSubsequence(string smaller, string larger) {
+//     int helper(string x, string y) {
+//         int m = x.size();
+//         int n = y.size();
+//         int dp[m+1][n+1];
+//         for(int i = 0; i<=m; i++)
+//         {
+//             dp[i][0]=0;
+//         }
+//         for(int i = 0; i<=n; i++)
+//         {
+//             dp[0][i]=0;
+//         }
+//         for(int i = 1; i<=m; i++){
+//             for(int j = 1; j<=n; j++){
+//                 if(x[i-1]==y[j-1]){
+//                     dp[i][j]=1+dp[i-1][j-1];
+//                 }else{
+//                     dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+//                 }
+//             }
+//         }
+//         return dp[m][n];
+//     }
+//     bool isSubsequence(string smaller, string larger) {
 
-        int x  = helper(smaller,larger);
-        if(x == smaller.size()){
-            return true;
-        }
-        return false;      
-    }
+//         int x  = helper(smaller,larger);
+//         if(x == smaller.size()){
+//             return true;
+//         }
+//         return false;      
+//     }
 };

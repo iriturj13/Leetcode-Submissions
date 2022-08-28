229b3f7d0bb6d9c6fc3562eda843f797d1033e16
Rituraj Anand <79366792+iriturj13@users.noreply.github.com>
2022-08-28 21:34:10 +0530

    Time: 16 ms (64.04%), Space: 9.5 MB (28.08%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        unordered_map<int, vector<int>> mp;
+        int m=mat.size();
+    int n=mat[0].size();
+    
+    for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
+            mp[i-j].push_back(mat[i][j]);
+        }
+    }
+    
+    for(int k=-(n-1);k<m;k++) {
+        sort(mp[k].begin(),mp[k].end());
+    }
+    
+    for(int i=m-1;i>=0;i--) {
+        for(int j=n-1;j>=0;j--) {
+            mat[i][j]=mp[i-j].back();
+            mp[i-j].pop_back();
+        }
+    }
+    return mat;
+        // officially the best keyboad out there
+    }
+};

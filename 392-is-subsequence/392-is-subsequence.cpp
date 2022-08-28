e5d8f65f5cf800c2c0a65e3270e05df8e041a977
Rituraj Anand <79366792+iriturj13@users.noreply.github.com>
2022-08-28 16:55:22 +0530

    Time: 149 ms (5.03%), Space: 286.2 MB (5.04%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    
+    
+    
+    
+         bool isSubSeq(string str1, string str2, int m, int n) 
+{ 
+    // Base Cases 
+    if (m == 0) return true; 
+    if (n == 0) return false; 
+    // If last characters of two strings are matching 
+    if (str1[m-1] == str2[n-1]) 
+        return isSubSeq(str1, str2, m-1, n-1); 
+    // If last characters are not matching 
+    return isSubSeq(str1, str2, m, n-1); 
+} 
     bool isSubsequence(string s, string t) {
         int m = s.size();
         int n = t.size();
-        int i = 0, j = 0;
-        while(i < m && j < n) {
-            if(s[i] == t[j]) {
-                i++;
-            }
-            j++;
-        }
-        return i == m ? 1 : 0;
+        return isSubSeq(s,t,m,n);
     }
+    // bool isSubsequence(string s, string t) {
+    //     int m = s.size();
+    //     int n = t.size();
+    //     int i = 0, j = 0;
+    //     while(i < m && j < n) {
+    //         if(s[i] == t[j]) {
+    //             i++;
+    //         }
+    //         j++;
+    //     }
+    //     return i == m ? 1 : 0;
+    // }
 //     int helper(string x, string y) {
 //         int m = x.size();
 //         int n = y.size();

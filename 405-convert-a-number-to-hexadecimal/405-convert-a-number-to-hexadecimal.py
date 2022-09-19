098c1839b9c2a3d2a3193c02e576ee5981a855ac
Rituraj Anand <79366792+iriturj13@users.noreply.github.com>
2022-09-19 15:23:30 +0530

    Time: 20 ms (68.47%), Space: 13.4 MB (34.23%) - LeetHub

---
+++ 405-convert-a-number-to-hexadecimal/405-convert-a-number-to-hexadecimal.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def toHex(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """  
+        
+        if num < 0:
+            num += 2 ** 32
+        
+        stack = []
+        s = "0123456789abcdef"
+        
+        while num:
+            stack.append(s[num % 16])
+            num //= 16
+        
+        if not stack:
+            return "0"
+        
+        stack.reverse()
+        return "".join(stack)
+        

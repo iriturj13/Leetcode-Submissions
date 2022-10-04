3214f00992a067318983154d8af7f317b95d637e
Rituraj Anand <79366792+iriturj13@users.noreply.github.com>
2022-10-04 09:53:01 +0530

    Time: 39 ms (11.60%), Space: 13.4 MB (83.13%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def interpret(self, command):
+        """
+        :type command: str
+        :rtype: str
+        """
+        return command.replace("()","o").replace("(al)","al")
+        

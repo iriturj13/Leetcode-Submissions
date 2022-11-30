7899bc4d86b2c7276398973847449aa8ebf8df04
Rituraj Anand <79366792+iriturj13@users.noreply.github.com>
2022-11-30 14:24:13 +0530

    Added solution - LeetHub

---
+++ Overlapping Intervals - GFG/overlapping-intervals.py
@@ -0,0 +1,38 @@
+class Solution:
+	def overlappedInterval(self, Intervals):
+	    res = []
+        
+        for interval in sorted(Intervals, key=lambda x:x[0]):
+            if res and interval[0] <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], interval[1])
+            else:
+                res.append(interval)
+        
+        return res
+
+
+
+
+#{ 
+ # Driver Code Starts
+		
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+    	n = int(input())
+    	a = list(map(int, input().strip().split()))
+    	Intervals = []
+    	j = 0
+    	for i in range(n):
+    		x = a[j]
+    		j += 1
+    		y = a[j]
+    		j += 1
+    		Intervals.append([x,y])
+    	obj = Solution()
+    	ans = obj.overlappedInterval(Intervals)
+    	for i in ans:
+    		for j in i:
+    			print(j, end = " ")
+    	print()
+# } Driver Code Ends
